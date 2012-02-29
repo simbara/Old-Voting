479af0d92c91bbf67f32a80f3c8e5159b5c1a4af
Cliff Chen <kurifuC4@gmail.com>
2012-02-29 15:09:16 -0800

    Added pyttsx speech module, added reviewBallot->contests transition, fixed the review_candidates->candidates transition

--- src/audioBallot/sampleBallot.py
+++ src/audioBallot/sampleBallot.py
@@ -1,9 +1,17 @@
+import pyttsx
 from fysom import Fysom
 import ParseModule
-from pyjamas import Window
+#from pyjamas import Window
 
 
+engine = pyttsx.init()
 race = ParseModule.initTree()
+
+def tts(text):
+    engine.stop()
+    print text
+    engine.say(text)
+    engine.runAndWait()
 
 def goToNextState(obj, pos, okToAdvance=True, contestPos=None):
     if fsm.current == 'contests':
@@ -22,9 +30,13 @@ def goToNextState(obj, pos, okToAdvance=True, contestPos=None):
         if obj is not None:
             fsm.nextContest(race=obj, pos=pos)
         else:
-            fsm.reviewBallot()
+            fsm.reviewBallot(race=obj, pos=pos)
     elif fsm.current == 'review_ballot':
-        fsm.doneBallot()
+        if okToAdvance:
+            fsm.doneBallot()
+        else:
+            fsm.reselectContest() #TODO
+        
         
 '''
 Traverse the list as provided by the 'obj', which can be either of type Race or Contest
@@ -34,18 +46,19 @@ whatever selection the user made in the 'Contest.userSelection' list
 def traverselist(obj, contestPos=None):
     alist = obj.selectionList
     pos = 0
-    print '*', alist[pos].name, 'highlighted *'
+    tts('* ' + alist[pos].name + ' highlighted *')
     while 1:
         nb = raw_input('\'y\' up, \'n\' down, \'h\' selects: ')
+        #if fsm.current == 'contests':
+        #    if nb.strip() == 'g':
+        #else:
         if nb.strip() == 'y':
             pos = (pos+1) if (pos+1<len(alist)) else 0
-        #elif nb.strip() == 'y':
-        #    pos
         elif nb.strip() == 'n':
             pos = len(alist)-1 if (pos-1<0) else pos-1
         elif nb.strip() == 'h':
-            break;
-        print '\t*', alist[pos].name, 'highlighted *'
+            break;    
+        tts('* ' + alist[pos].name + ' highlighted *')
 
     if fsm.current == 'candidates':
         #print 'CANDIDATES LOOK: obj is', obj.name, 'pos is ', pos, 'contestPos is ', contestPos
@@ -64,29 +77,28 @@ def onintro(e):
     print 'hello!'
     
 def oncontests(e): 
-    print '\nThe contests are:'
+    tts('\nThe contests are:')
     for i, contest in zip(range(len(race.selectionList)), race.selectionList):
-        print '\t' + str(i+1) + ') ' + contest.name 
+        tts('\t' + str(i+1) + ') ' + contest.name) 
     traverselist(race)
 
 # e.pos: the current Contest, which is the position in the race.selectionList
 def oncandidates(e): 
-    print '\nCurrent race is: ', e.race.name
-    print 'Candidates are:'
+    tts('\nCurrent race is: ' + e.race.name)
+    tts('Candidates are:')
     currContest = race.selectionList[e.contestPos]
     for i, person in zip(range(len(currContest.selectionList)), currContest.selectionList):
-        print "\t" + str(i+1) + ') ' + person.name
+        tts("\t" + str(i+1) + ') ' + person.name)
     traverselist(currContest, contestPos=e.contestPos)
 
 # e.pos: the user's selection in the position of the list
 def onreviewcandidates(e):
-    print '\nReview Your Choice for', e.choice.name, ':'
+    tts('\nReview Your Choice for ' + e.choice.name + ':')
     candidate = e.choice.selectionList[e.pos]
-    print '\t', candidate.name
+    tts('\t' + candidate.name)
     
-    # even: yes, odd: no
     currChoice = 0
-    print '* <YES> highlighted *'
+    tts('* <YES> highlighted *')
     while 1:
         nb = raw_input('\'y\' up, \'n\' down, \'h\' selects: ')
         if nb.strip() == 'y':
@@ -94,18 +106,19 @@ def onreviewcandidates(e):
         elif nb.strip() == 'n':
             currChoice -= 1
         elif nb.strip() == 'h':
+            tts("\t* CONFIRMED")
             break;
         if currChoice % 2 == 0:
-            print '\t* <YES> highlighted *'
+            tts('\t* <YES> highlighted *')
         else:
-            print '\t* <NO> highlighted *'
+            tts('\t* <NO> highlighted *')
         
     if currChoice % 2 == 0:
         e.choice.userSelection[:] = []
         e.choice.userSelection.append(e.pos)
         goToNextState(None, None, okToAdvance=True)
     else:
-        goToNextState(e.choice, e.pos, contestPos=e.contestPos)
+        goToNextState(e.choice, e.pos, contestPos=e.contestPos, okToAdvance=False)
 
 def oncheckdone(e):
     for i, contest in zip(range(len(race.selectionList)), race.selectionList):
@@ -114,14 +127,36 @@ def oncheckdone(e):
             return
     goToNextState(None, None)
 
+# fsm.reviewBallot(race=obj, pos=pos)
 def onreviewballot(e):
-    print '\nReview your selections:'
+    tts('\nReview your selections:')
     for contest in race.selectionList:
-        print 'Contest: ', contest.name, ':', contest.selectionList[contest.userSelection[0]].name
-    goToNextState(None, None)
+        tts(contest.name + ':' + contest.selectionList[contest.userSelection[0]].name)
+        
+    currChoice = 0
+    tts('* <YES> highlighted *')
+    while 1:
+        nb = raw_input('\'y\' up, \'n\' down, \'h\' selects: ')
+        if nb.strip() == 'y':
+            currChoice += 1
+        elif nb.strip() == 'n':
+            currChoice -= 1
+        elif nb.strip() == 'h':
+            tts("\t* CONFIRMED")
+            break;
+        if currChoice % 2 == 0:
+            tts('\t* <YES> highlighted *')
+        else:
+            tts('\t* <NO> highlighted *')
+        
+    if currChoice % 2 == 0:
+        goToNextState(None, None)
+    else:
+        goToNextState(e.race, e.pos, okToAdvance=False) # TODO
+    
     
 def ondoneballot(e):
-    print '\nVoting complete! Thanks for using this system!'
+    tts('\nVoting complete! Thanks for using this system!')
 
 '''
 States and Events
@@ -135,7 +170,9 @@ fsm = Fysom({
     {'name': 'reselectCandidates', 'src': 'review_candidates', 'dst': 'candidates'},
     {'name': 'doneReview',  'src': 'review_candidates',   'dst': 'check_done'},
     {'name': 'nextContest', 'src': 'check_done',  'dst': 'contests'},
+    {'name': 'otherContest', 'src': 'contests',  'dst': 'contests'}, # TODO
     {'name': 'reviewBallot', 'src': 'check_done', 'dst': 'review_ballot'},
+    {'name': 'reselectContest', 'src': 'review_ballot', 'dst': 'contests'}, # TODO
     {'name': 'doneBallot', 'src': 'review_ballot', 'dst': 'done_ballot'},    
   ],
     'callbacks': {
